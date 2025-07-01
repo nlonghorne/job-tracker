5e3b2383cc6be336a2e231535b60f8109e21d435
nlonghorne <nlonghorne@gmail.com>
2025-07-01 16:55:13 +1000

    First commit

---
+++ src/test/java/com/nlonghorne/job_tracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlonghorne.job_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
